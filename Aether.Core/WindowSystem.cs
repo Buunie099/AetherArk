f95c61ad5bba8949c1fd245468a7428d00ab8602
Ben Dake <124413509+Buunie099@users.noreply.github.com>
2026-05-05 16:26:05 -0400

    blehhh

---
+++ Aether.Core/WindowSystem.cs
@@ -0,0 +1,14 @@
+using System.Windows;
+
+namespace AetherArk.Aether.Core
+{
+    public class WindowSystem : Window
+    {
+        public WindowSystem()
+        {
+            this.Title = "Blehhh";
+            this.Width = 400;
+            this.Height = 400;
+        }
+    }
+}
---
+++ Core.cs
@@ -0,0 +1,10 @@
+namespace AetherArk
+{
+    public class Core
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
